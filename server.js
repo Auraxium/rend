274ee5e4b02b0df9862fe46b584217bad5fad914
= <=>
2023-03-26 16:25:38 -0700

    final

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
-//#region INITIAL
+//#region ----------------INIT---------------------
 const express = require("express");
 const app = express();
 var cors = require("cors");
 var { google } = require("googleapis");
-var OAuth2 = google.auth.OAuth2;
 const axios = require("axios");
-const chromium = require("chromium");
-const puppeteer = require("puppeteer");
 const mongoose = require("mongoose");
-var dataModel = mongoose.model("Data", new mongoose.Schema({ data: {} }));
+const { ObjectId } = require('mongodb');
+var dataModel = mongoose.model("Account", new mongoose.Schema({_id: {}, config: {}, songs: {}, playlists: {}}));
+const SpotifyWebApi = require("spotify-web-api-node");
+const fs = require("fs");
+
 const URI =
   "mongodb+srv://Auraxium:fyeFDEQCZYydeMnR@cluster0.hcxjp2q.mongodb.net/?retryWrites=true&w=majority";
 
 const YT_API_KEY = "AIzaSyCMzBshD58xKBIubjVhfjjn1jmvSA7_Ex0";
 const baseApiUrl = "https://www.googleapis.com/youtube/v3";
-
-var opts = {
-  maxResults: 10,
-  key: "AIzaSyCa-ozYT_nemTy1dYHDBKBUX1qdwUlZSx0",
-};
-
-p = (s) => console.log(s);
 
 mongoose
   .connect(URI, {
@@ -29,6 +23,10 @@ mongoose
   })
   .then(() => console.log("connected to database"))
   .catch((err) => console.log(err));
+
+//#endregion
+
+//#region -------------FUNCTIONS------------------
 
 function delay(secs) {
   return new Promise((resolve) => {
@@ -62,6 +60,10 @@ function parseDuration(s) {
   return str.join("");
 }
 
+//#endregion
+
+//#region ------------SERVER INIT------------------
+
 app.use(
   cors({
     origin: "*",
@@ -85,32 +87,134 @@ app.get("/native", (req, res) => {
   res.sendFile(__dirname + "/songs.json", (err) => console.log(err));
 });
 
-app.get("/load", (req, res) => {
-  dataModel
-    .findOne()
-    .then((data) => res.json(data.data))
+app.post("/mognoInit", (req, res) => {});
+
+app.post("/load", (req, res) => {
+  dataModel.findById(req.body._id)
+    .then((data) => res.json(data))
     .catch((err) => res.json("Error: " + err));
+	res.status(500)
 });
 
 app.post("/save", (req, res) => {
-  dataModel.findOne().then((response) => {
-    if (response == null) {
+	//console.log(req.body)
+  dataModel.findById(req.body._id).then((mg) => {
+    if (!mg) {
       let init = new dataModel(req.body);
-      init.save();
+      init.save().catch(console.log);
     } else {
-      response.data = req.body;
-      response
-        .save()
+      mg.data = req.body;
+      mg.save()
         .then(() => res.json("songs updated!"))
         .catch((err) => res.status(400).json("Error: " + err));
     }
-  });
+  }).catch(console.log);
+
+  // dataModel.findOne().then((response) => {
+  //   if (response == null) {
+  //     let init = new dataModel(req.body);
+  //     init.save();
+  //   } else {
+  //     response.data = req.body;
+  //     response
+  //       .save()
+  //       .then(() => res.json("songs updated!"))
+  //       .catch((err) => res.status(400).json("Error: " + err));
+  //   }
+  // });
 });
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, null, () => console.log("Running on " + PORT));
+
+//#endregion
+
+//#region ---------------GOOGLE---------------------
+
+const googCID =
+  "482771111816-0fpbmptbpflo8ackjf70gl1ls3ejl7fi.apps.googleusercontent.com";
+const googCS = "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";
+
+let googCache = {};
+
+const GOauth = new google.auth.OAuth2(
+  googCID,
+  googCS,
+  "http://localhost:8080/googOauth/callback"
+);
+
+app.post("/googOauth", (req, res) => {
+  googCache[req.body.uuid] = { origin: req.body.origin };
+  const googAuthUrl = GOauth.generateAuthUrl({
+    access_type: "offline",
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+    include_granted_scopes: true,
+    state: req.body.uuid,
+  });
+  res.json({ url: googAuthUrl });
+});
+
+app.get("/googOauth/callback", async (req, res) => {
+  console.log(req.query);
+  session = req.query.state;
+  const response = await GOauth.getToken(req.query.code);
+  GOauth.setCredentials({
+    access_token: response.tokens.access_token,
+    refresh_token: response.tokens.refresh_token,
+  });
+  googCache[session]["access_token"] = response.tokens.access_token;
+  googCache[session]["refresh_token"] = response.tokens.refresh_token;
+  let ax = await axios(
+    "https://people.googleapis.com/v1/people/me?personFields=names",
+    {
+      headers: {
+        Authorization: `Bearer ${response.tokens.access_token}`,
+      },
+    }
+  );
+  googCache[session]["googleId"] = ax.data.names[0].metadata.source.id;
+  googCache[session]["username"] = ax.data.names[0].displayName;
+  googCache[session]["now"] = Date.now();
+  res.redirect(googCache[session]["origin"]);
+});
+
+app.post("/googGetToken", (req, res) => {
+  let token = googCache[req.body.uuid];
+  delete googCache[req.body.uuid];
+  return res.json(token);
+});
+
+//#endregion
+
+//#region ---------------YOUTUBE--------------------
 
 app.get("/yttest", (req, res) => {
   axios(
     `${baseApiUrl}/videos?id=b8-tXG8KrWs,hZvitm7eK9c,MrLnOfYFVvQ,lcNfSwl2SmI,jcxate72OMg,oIoyTuvqHAs,cW6uJAaLfRA,xwhBRJStz7w,qn7HvnMJZd4,RDk5NB3pgJo,mg-ODPxYl9Q,CDhUvkzLsmQ,a2Cenb4UNFE,vE8zTxbifNM,yFdLSM8zVVI,uxurZPG1k-M,tRAEkH3EqGQ,MBq722OJ8QY,C6mvSrZA410,cbuV0WtQXjw,j2VtMsBYTjI&part=contentDetails&part=snippet&key=${YT_API_KEY}`
   ).then((data) => res.json(data.data));
+});
+
+app.post("/YTsearch", async (req, res) => {
+  console.log(req.body.search);
+  let vids;
+  await axios(
+    `${baseApiUrl}/search?key=${YT_API_KEY}
+		&type=video
+		&part=snippet
+		&maxResults=7
+		&q=${req.body.search.replace(/\s+/g, "+")}`
+  ).then((data) => (vids = data.data.items));
+
+  let send = vids.map((e) => {
+    return {
+      name: e["snippet"]["title"],
+      id: e["id"]["videoId"],
+    };
+  });
+  res.json(send);
 });
 
 app.post("/getYTData", async (req, res) => {
@@ -131,9 +235,16 @@ app.post("/getYTData", async (req, res) => {
 
       for (let j = save; j < i; j++) {
         for (let k = 0; k < searches[j].length; k++) {
-          searches[j][k]["duration"] =
-            part[count]["contentDetails"]["duration"] ? part[count]["contentDetails"]["duration"] : '0:00';
-          count++;
+          try {
+            searches[j][k]["duration"] = part[count]["contentDetails"][
+              "duration"
+            ]
+              ? part[count]["contentDetails"]["duration"]
+              : "0:00";
+            count++;
+          } catch (err) {
+            continue;
+          }
         }
       }
       save = i;
@@ -154,7 +265,7 @@ app.post("/getYTData", async (req, res) => {
       `${baseApiUrl}/videos?id=${str}&part=contentDetails&part=snippet&key=${YT_API_KEY}`
     );
 
-    let part = request["data"]['items'];
+    let part = request["data"]["items"];
     console.log(part);
     console.log(str);
 
@@ -172,82 +283,100 @@ app.post("/getYTData", async (req, res) => {
   res.json({ searches: searches });
 });
 
-// yt.search('hello').then(res => console.log(res))
+//#endregion
 
-app.post("/YTsearch", async (req, res) => {
-  console.log(req.body.search);
-  let vids;
-  await axios(
-    `${baseApiUrl}/search?key=${YT_API_KEY}
-		&type=video
-		&part=snippet
-		&maxResults=7
-		&q=${req.body.search.replace(/\s+/g, "+")}`
-  ).then((data) => (vids = data.data.items));
+//#region  --------------SPOTIFY------------------
 
-  // for (let i = 0; i < vids.length; i++) {
-  //   let duration = await axios(
-  //     `${baseApiUrl}/videos?id=${vids[i]["id"]["videoId"]}&part=contentDetails&part=snippet&key=${YT_API_KEY}`
-  //   );
+const spotifyCID = "66e36ea2ef934f62b4cb680d7bacae1a";
+const spotifyCS = "f1c16266ebc54950b32d8bad98109e72";
 
-  //   vids[i]["duration"] =
-  //     duration["data"]["items"][0]["contentDetails"]["duration"];
-  // }
+let spotCache = {};
 
-  let send = vids.map((e) => {
-    return {
-      name: e["snippet"]["title"],
-      id: e["id"]["videoId"],
-    };
-  });
-
-  // console.log(send);
-  res.json(send);
+const spotifyApi = new SpotifyWebApi({
+  clientId: spotifyCID,
+  clientSecret: spotifyCS,
+  redirectUri: "http://localhost:8080/spotifyOauth/callback",
 });
 
-app.post("/SpotifyPlaylist", async (req, res) => {
-  const browser = await puppeteer.launch({
-    executablePath: chromium.path,
-    headless: true,
-  });
-  const page = await browser.newPage();
-  await page.goto(req.body.search, { timeout: 90000 });
-  console.log("SP started");
+app.post("/spotGetToken", (req, res) => {
+  let token = spotCache[req.body.uuid];
+  delete spotCache[req.body.uuid];
+  return res.json(token);
+});
 
-  await new Promise((res, rej) => setTimeout(() => res(""), 4000));
+app.post("/spotGetAccess", async (req, res) => {
+  axios
+    .post("https://www.googleapis.com/oauth2/v4/token", {
+      client_id: spotifyCID,
+      client_secret: spotifyCS,
+      refresh_token: req.body.rt,
+      grant_type: "refresh_token",
+    })
+    .then((ax) => res.json({ ac: ac }))
+    .catch((err) => res.json(err));
+});
 
-  const num = await page.$$eval(`meta[property="og:description"]`, (e) =>
-    e.map((el) => el.getAttribute("content"))
+app.post("/spotOauth", async (req, res) => {
+  spotCache[req.body.uuid] = { origin: req.body.origin };
+  const authorizeUrl = spotifyApi.createAuthorizeURL(
+    ["playlist-read-collaborative"],
+    req.body.uuid
   );
-  let length = +num[0].replace(/\D+/g, "");
-  let songs = [];
-  console.log(length);
-
-  for (let i = 2; i < +length + 2; i++) {
-    let song = await page.$$eval(`[aria-rowindex="${i}"]`, (e) =>
-      e.map((el) => ({
-        name: el.querySelector('div [aria-colindex="2"] div a div').innerText,
-        artist: el.querySelector('div [aria-colindex="2"] div span a')
-          .innerText,
-        length: el.querySelector('div [aria-colindex="5"] div').innerText,
-      }))
-    );
-
-    let scroll = await page.$(`[aria-rowindex="${i}"]`);
-    if (!scroll) break;
-    await scroll.evaluate((element) => element.scrollIntoView());
-    await new Promise((res, rej) => setTimeout(() => res(""), 100));
-
-    songs.push(song[0]);
-    console.log(i + " songs");
-  }
-
-  console.log(songs);
-  await browser.close();
-  res.send(songs);
+  res.json({ url: authorizeUrl });
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, null, () => console.log("Running on " + PORT));
+app.get("/spotifyOauth/callback", (req, res) => {
+  let state = req.query.state;
+  spotifyApi
+    .authorizationCodeGrant(req.query.code)
+    .then((data) => {
+      console.log(data);
+      spotCache[state]["access_token"] = data.body["access_token"];
+      spotCache[state]["refresh_token"] = data.body["refresh_token"];
+      spotCache[state]["expires"] = data.body["expires_in"];
+      spotCache[state]["now"] = Date.now();
+    })
+    .finally(() => res.redirect(spotCache[state]["origin"]));
+});
 
 //#endregion
+
+// const browser = await puppeteer.launch({
+//   executablePath: chromium.path,
+//   headless: true,
+// });
+// const page = await browser.newPage();
+// await page.goto(req.body.search, { timeout: 90000 });
+// console.log("SP started");
+
+// await new Promise((res, rej) => setTimeout(() => res(""), 4000));
+
+// const num = await page.$$eval(`meta[property="og:description"]`, (e) =>
+//   e.map((el) => el.getAttribute("content"))
+// );
+// let length = +num[0].replace(/\D+/g, "");
+// let songs = [];
+// console.log(length);
+
+// for (let i = 2; i < +length + 2; i++) {
+//   let song = await page.$$eval(`[aria-rowindex="${i}"]`, (e) =>
+//     e.map((el) => ({
+//       name: el.querySelector('div [aria-colindex="2"] div a div').innerText,
+//       artist: el.querySelector('div [aria-colindex="2"] div span a')
+//         .innerText,
+//       length: el.querySelector('div [aria-colindex="5"] div').innerText,
+//     }))
+//   );
+
+//   let scroll = await page.$(`[aria-rowindex="${i}"]`);
+//   if (!scroll) break;
+//   await scroll.evaluate((element) => element.scrollIntoView());
+//   await new Promise((res, rej) => setTimeout(() => res(""), 100));
+
+//   songs.push(song[0]);
+//   console.log(i + " songs");
+// }
+
+// console.log(songs);
+// await browser.close();
+// res.send(songs);
